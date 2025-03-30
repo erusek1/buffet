3172b3dbf2420f4ac1ac7019a3b3c2c166bae56c
erusek1 <erik@erikrusekelectric.com>
2025-03-30 18:39:09 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
-<<<<<<< HEAD
-import App from '../App';
-=======
 import App from './App';
->>>>>>> 2603ff5c7621d8d23627fbadb1f1661f2c4f2ace
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 2603ff5c7621d8d23627fbadb1f1661f2c4f2ace
